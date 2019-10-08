b6981e78934228f42a61b553822584d978e227c2
Martin Oppitz <npmjs@martinoppitz.com>
2019-10-08 04:36:37 +0200

    framework offset

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,77 @@
 const path = require('path');
+// const util = require('util');
 
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const packageJson = require('./package.json');
 
-const config = {
-  entry: './src/index.ts',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?(j|t)s$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
-  },
-  plugins: [
-    new CopyWebpackPlugin([{ from: 'src/assets', to: 'assets' }]),
-    new HtmlWebpackPlugin({
-      name: packageJson.name,
-      template: 'index.html',
-      title: packageJson.description,
-      version: packageJson.version
-    })
-  ]
-};
-
 module.exports = (env, argv) => {
+  const babelConfigPlugins = [];
+  const babelConfigPresets = [];
+  switch (env.framework) {
+    case 'angular':
+      break;
+    case 'inferno':
+      babelConfigPlugins.push([
+        'babel-plugin-inferno',
+        {
+          imports: true
+        }
+      ]);
+      break;
+    case 'react':
+      babelConfigPresets.push('@babel/preset-react');
+      break;
+    case 'vue':
+      babelConfigPresets.push('@vue/app');
+      break;
+    default:
+      env.framework = 'angularjs';
+      babelConfigPlugins.push('angularjs-annotate');
+  }
+
+  const config = {
+    entry: path.join(__dirname, 'src', `main.${env.framework}.ts`),
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?(j|t)s$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+              plugins: babelConfigPlugins,
+              presets: babelConfigPresets
+            }
+          }
+        }
+      ]
+    },
+    plugins: [
+      new CopyWebpackPlugin([{ from: 'src/assets', to: 'assets' }]),
+      new HtmlWebpackPlugin({
+        name: packageJson.name,
+        template: 'index.html',
+        title: packageJson.description,
+        version: packageJson.version
+      })
+    ]
+  };
+
   switch (argv.mode) {
     case 'production':
       break;
     default:
       config.devtool = 'source-map';
   }
+
+  // console.log(util.inspect(config, true, null, true));
+
   return config;
 };
