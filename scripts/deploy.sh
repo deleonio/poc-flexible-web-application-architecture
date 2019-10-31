cb225d1f96c2e2567a94f57aa282d79995c8b381
Martin Oppitz <npmjs@martinoppitz.com>
2019-10-31 23:56:09 +0100

    ignore host key validation

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,3 @@
 export SSHPASS=${SFTP_PASSWORD}
+ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" ${SFTP_USER}@${SFTP_HOST}
 sshpass -e sftp -oBatchMode=no -b scripts/batchfile.sftp ${SFTP_USER}@${SFTP_HOST}:poc
