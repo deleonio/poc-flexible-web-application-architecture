bbabe66bcd17c5c3dab2553ad64d4a0515bc1296
Martin Oppitz <m.oppitz@modevel.de>
2019-11-04 22:52:43 +0100

    app comp aurelia

--- src/aurelia.ts
+++ src/aurelia.ts
@@ -1,6 +1,14 @@
 import { bootstrap } from 'aurelia-bootstrapper';
 import { Aurelia } from 'aurelia-framework';
 import { PLATFORM } from 'aurelia-pal';
+import { DI } from 'shares/injector';
+
+console.log(Aurelia);
+
+DI.register('Framework', {
+  name: 'Aurelia',
+  version: '1.3.1'
+});
 
 export function configure(aurelia: Aurelia) {
   aurelia.use.standardConfiguration().developmentLogging();
--- src/components/app/component.aurelia.ts
+++ src/components/app/component.aurelia.ts
@@ -1,3 +1,18 @@
+import { AppController } from './controller';
+
 export class App {
-  public message: string = 'Hello World!';
+  protected $ctrl: any = null;
+
+  constructor() {
+    this.bindControllerLogic(new AppController());
+  }
+
+  private bindControllerLogic($ctrl: Object) {
+    if (typeof $ctrl === 'object' && $ctrl !== null) {
+      try {
+        this.$ctrl = $ctrl;
+        // tslint:disable-next-line: no-empty
+      } catch (error) {}
+    }
+  }
 }
