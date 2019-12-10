d66361862ddee8c21cd4ae77986f5958216a3dc6
Martin Oppitz <npmjs@martinoppitz.com>
2019-12-11 00:10:06 +0100

    add nightwatch

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -5,12 +5,26 @@ module.exports = {
   webdriver: {
     start_process: true,
     server_path: chromedriver.path,
-    port: 9515
+    port: 9515,
+    cli_args: ['--verbose']
   },
   test_settings: {
     default: {
       desiredCapabilities: {
-        browserName: 'chrome'
+        browserName: 'chrome',
+        javascriptEnabled: true,
+        acceptSslCerts: true,
+        chromeOptions: {
+          args: ['disable-web-security', 'no-sandbox', 'start-maximized']
+        }
+      }
+    },
+    headless: {
+      desiredCapabilities: {
+        browserName: 'chrome',
+        chromeOptions: {
+          args: ['disable-web-security', 'headless', 'no-sandbox', 'start-maximized']
+        }
       }
     }
   }
