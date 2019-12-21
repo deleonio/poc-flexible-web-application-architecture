ae8019159e29f260cde0999b5185d3bd29d4f1df
Martin Oppitz <npmjs@martinoppitz.com>
2019-12-22 00:30:27 +0100

    angular@9.0.0-rc7 breaking change

--- src/components/series/edit/component.angular.ts
+++ src/components/series/edit/component.angular.ts
@@ -1,27 +1,28 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 import { EditSerieController } from './controller';
 
 @Component({
   selector: 'edit-serie',
   template: `
-    <form (ngSubmit)="onSubmit()">
+    <form (ngSubmit)="$ctrl.onSubmit()">
       <h5>Edit a existing measuring serie</h5>
-      <editor-serie [editorForm]="editorForm"></editor-serie>
+      <editor-serie [editorForm]="$ctrl.editorForm"></editor-serie>
       <button class="btn btn-primary" type="submit" id="submit">Edit</button>
-      <button class="btn btn-secondary" type="reset" id="cancel" (click)="onCancel()">
-        Abbrechen
+      <button class="btn btn-secondary" type="reset" id="cancel" (click)="$ctrl.onCancel()">
+        Abbrechen Abbrechen
       </button>
-      <button class="btn btn-danger" id="delete" type="button" (click)="onDelete()">
+      <button class="btn btn-danger" id="delete" type="button" (click)="$ctrl.onDelete()">
         Delete
       </button>
     </form>
   `
 })
-export class EditSerieComponent extends EditSerieController implements OnChanges {
+export class EditSerieComponent implements OnChanges {
+  public $ctrl: EditSerieController = new EditSerieController(null);
   @Input() public resolvedRoute: any;
 
   public ngOnChanges() {
-    this.changeMeasuredSerie(this.resolvedRoute.params.id);
+    this.$ctrl.changeMeasuredSerie(this.resolvedRoute.params.id);
   }
 }
