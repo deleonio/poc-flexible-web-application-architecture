5ff3496613806274e5d7a5fe11aa0eaf61e6683d
Martin Oppitz <npmjs@martinoppitz.com>
2020-01-10 08:34:03 +0100

    fix naming svelte

--- src/svelte.ts
+++ src/svelte.ts
@@ -2,7 +2,7 @@ import App from './components/app/component.svelte';
 import { DI } from './shares/injector';
 
 DI.register('Framework', {
-  name: 'Sverte',
+  name: 'Svelte',
   version: require('svelte/package.json').version
 });
 require('./shares/register');
