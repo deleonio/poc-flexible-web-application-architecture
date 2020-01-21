bb8408ef82f4adeb096d0565dc8a0253e7885554
Martin Oppitz <npmjs@martinoppitz.com>
2020-01-21 05:37:48 +0100

    revert svelte tpl and adapt cli changes

--- src/components/series/list/component.svelte
+++ src/components/series/list/component.svelte
@@ -1,30 +1,79 @@
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+<script>
+  import { ListSerieController } from './controller';
+  import CreateSerieComponent from '../create/component.svelte';
+  import EditSerieComponent from '../edit/component.svelte';
+  const ctrl = new ListSerieController();
+  let elementsRef = ctrl.elements;
+  let durationRef = ctrl.duration;
+  let measuredSeriesRef = ctrl.measuredSeries;
+  let measuredSerieRef = ctrl.measuredSerie;
+  let showPerformanceButtonRef = ctrl.showPerformanceButton;
+  ctrl.renderView = () => {
+    measuredSeriesRef = ctrl.measuredSeries;
+    elementsRef = ctrl.elements;
+    durationRef = ctrl.duration;
+    showPerformanceButtonRef = ctrl.showPerformanceButton;
+  };
+</script>
+
+<div>
+  <h5>List</h5>
+  <div>
+    <button
+      class="btn btn-success"
+      id="add"
+      type="button"
+      on:click={() => {
+        ctrl.add();
+      }}>
+      Add
+    </button>
+    {#if showPerformanceButtonRef}
+      <button
+        class="btn btn-info"
+        id="start"
+        type="button"
+        on:click={() => {
+          ctrl.onStart();
+        }}>
+        Performance
+      </button>
+    {/if}
+  </div>
+  {#each elementsRef as serie, index (index)}
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">ID</th>
+          <th scope="col">Title</th>
+          <th scope="col">Unit</th>
+          <th scope="col">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {#each measuredSeriesRef as serie, index (index)}
+          <tr>
+            <td>{index + 1}</td>
+            <td>{serie.getId()}</td>
+            <td>{serie.getTitle()}</td>
+            <td>{serie.getUnit()}</td>
+            <td>
+              <button
+                class="btn btn-info"
+                id="edit-{index}"
+                type="button"
+                on:click={() => {
+                  ctrl.edit(serie);
+                  measuredSerieRef = ctrl.measuredSerie;
+                }}>
+                Edit
+              </button>
+            </td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  {/each}
+  <small>Duration: {durationRef} ms</small>
+</div>
