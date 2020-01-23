c2c242f72276667d65652e92da71d0b0a72aed83
Martin Oppitz <npmjs@martinoppitz.com>
2020-01-23 06:13:28 +0100

    refact test folder

---
+++ test/e2e/happy-path.js
@@ -0,0 +1,20 @@
+module.exports = {
+  'Test demo': browser => {
+    browser
+      // .url('http://localhost:8080/')
+      .url('https://github.modevel.de/poc')
+      .waitForElementVisible('div#app')
+      // .waitForElementVisible('input#new_title')
+      // .waitForElementVisible('input#new_unit')
+      // .setValue('input#new_title', 'nightwatch')
+      // .setValue('input#new_unit', 'nightwatch')
+      // .click('button#submit-add')
+      // .waitForElementVisible('button#submit-add')
+      // .click('button#edit-0')
+      // .setValue('input#edit_title', 'nightwatch')
+      // .setValue('input#edit_unit', 'nightwatch')
+      // .click('button#submit-edit')
+      // .click('button#delete-0')
+      .end();
+  }
+};
