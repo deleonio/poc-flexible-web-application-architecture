3ffbbaa4244f4143972dc4ce467b444d58b22c39
Martin Oppitz <npmjs@martinoppitz.com>
2020-01-31 10:38:55 +0100

    fix workbox config

--- workbox-config.js
+++ workbox-config.js
@@ -1,4 +1,10 @@
 module.exports = {
   include: ['**/*.{css,gz,js,png,html,json}'],
-  swDest: 'public/sw.js'
+  swDest: 'public/sw.js',
+  runtimeCaching: [
+    {
+      urlPattern: /\.(css|gz|js|png|html|json)$/,
+      handler: 'CacheFirst'
+    }
+  ]
 };
