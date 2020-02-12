0ffbd767fd8277623d80392018d0058cc34a583e
Martin Oppitz <npmjs@martinoppitz.com>
2020-02-13 00:11:19 +0100

    nightwatch

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,1 +1,47 @@
-module.exports = require('@leanup/cli/nightwatch.conf');
+const path = require('path');
+
+module.exports = {
+  src_folders: ['tests/e2e'],
+  globals_path: './nightwatch.global.js',
+  webdriver: {
+    start_process: true,
+    server_path: require('chromedriver').path,
+    port: 9515
+  },
+  test_settings: {
+    default: {
+      desiredCapabilities: {
+        browserName: 'chrome',
+        chromeOptions: {
+          w3c: false
+        }
+      }
+    },
+    selenium: {
+      // Selenium Server is running locally and is managed by Nightwatch
+      selenium: {
+        port: 4444,
+        host: 'triton927.startdedicated.de'
+      },
+      webdriver: {
+        start_process: false,
+        port: 4444
+      }
+    },
+    'selenium.chrome': {
+      extends: 'selenium',
+      desiredCapabilities: {
+        browserName: 'chrome',
+        chromeOptions: {
+          w3c: false
+        }
+      }
+    },
+    'selenium.firefox': {
+      extends: 'selenium',
+      desiredCapabilities: {
+        browserName: 'firefox'
+      }
+    }
+  }
+};
---
+++ tests/e2e/ecosia.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'Demo test ecosia.org': function(browser) {
+    browser
+      .url('https://www.ecosia.org/')
+      .waitForElementVisible('body')
+      .assert.titleContains('Ecosia')
+      .assert.visible('input[type=search]')
+      .setValue('input[type=search]', 'nightwatch')
+      .assert.visible('button[type=submit]')
+      .click('button[type=submit]')
+      .assert.containsText('.mainline-results', 'Nightwatch.js')
+      .end();
+  }
+};
