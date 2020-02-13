fbb55f37d1f3a4b345599bf500a6823625578fb8
Martin Oppitz <martinoppitz@users.noreply.github.com>
2020-02-13 12:59:14 +0100

    Update nightwatch.global.js

--- nightwatch.global.js
+++ nightwatch.global.js
@@ -1,5 +1,19 @@
-const allure2 = require("nightwatch-allure2-adapter");
-
 module.exports = {
-  reporter: allure2.write
+  /**
+   * After all the tests are run, evaluate if there were errors and exit appropriately.
+   *
+   * If there were failures or errors, exit 1, else exit 0.
+   *
+   * @param results
+   */
+  reporter: function(results) {
+    if (
+      (typeof results.failed === 'undefined' || results.failed === 0) &&
+      (typeof results.error === 'undefined' || results.error === 0)
+    ) {
+      process.exit(0);
+    } else {
+      process.exit(1);
+    }
+  }
 };
