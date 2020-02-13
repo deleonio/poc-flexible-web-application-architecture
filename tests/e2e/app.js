44018aeea9a9d0f27493f177bca090eaed5913f5
Martin Oppitz <martinoppitz@users.noreply.github.com>
2020-02-13 09:32:08 +0100

    Create app.js

---
+++ tests/e2e/app.js
@@ -0,0 +1,53 @@
+const ACTION_DELAY = 1500;
+
+function testFall(host) {
+  return (browser) => {
+    console.log(`http://${host}:8080`);
+    try {
+      browser
+      .url(`http://${host}:8080`)
+      .pause(ACTION_DELAY)
+      .waitForElementVisible('h1')
+      .pause(ACTION_DELAY)
+      .click('#edit-0')
+      .pause(ACTION_DELAY)
+      .clearValue ('#edit_title')
+      .pause(ACTION_DELAY)
+      .setValue('#edit_title', 'Baum')
+      .pause(ACTION_DELAY)
+      .click('#submit')
+      .pause(ACTION_DELAY)
+      .click('#add')
+      .pause(ACTION_DELAY)
+      .setValue('#new_title', 'Äpfel')
+      .pause(ACTION_DELAY)
+      .setValue('#new_unit', 'Pack')
+      .pause(ACTION_DELAY)
+      .click('#submit')
+      .pause(ACTION_DELAY)
+      .click('#edit-0')
+      .pause(ACTION_DELAY)
+      .click('#delete')
+      .pause(ACTION_DELAY)
+      .click('#edit-1')
+      .pause(ACTION_DELAY)
+      .click('#delete')
+      .pause(ACTION_DELAY)
+      .click('#edit-2')
+      .pause(ACTION_DELAY)
+      .click('#delete')
+      .pause(ACTION_DELAY)
+      .click('#start')
+      .pause(60000)
+      .end();
+    } catch (e) {}
+  };
+}
+
+let tests = {};
+[
+  'https://github.modevel.de/poc/'
+].forEach((host) => {
+  tests[host] = testFall(host);
+});
+module.exports = tests;
