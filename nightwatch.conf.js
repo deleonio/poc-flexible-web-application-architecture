33b19c1010b3c32e9488a2060d8f1f57dbd4970e
Martin Oppitz <npmjs@martinoppitz.com>
2020-02-15 07:20:58 +0100

    e2e test

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,43 +1,77 @@
-const path = require('path');
+const COMMON = {
+  acceptInsecureCerts: true,
+  javascriptEnabled: true
+};
+const CHROME = {
+  ...COMMON,
+  browserName: 'chrome',
+  chromeOptions: {
+    w3c: false
+  }
+};
+const FIREFOX = {
+  ...COMMON,
+  browserName: 'firefox'
+};
 
 module.exports = {
   src_folders: ['tests/e2e'],
   globals_path: './nightwatch.global.js',
-  webdriver: {
-    start_process: true,
-    server_path: require('chromedriver').path,
-    port: 9515
-  },
   test_workers: {
-    "enabled": true,
-    "workers": "auto"
+    enabled: true,
+    workers: 'auto'
   },
   test_settings: {
+    localChrome: {
+      webdriver: {
+        start_process: true,
+        server_path: require('chromedriver').path,
+        port: 9515
+      },
+      desiredCapabilities: CHROME
+    },
+    localFirefox: {
+      webdriver: {
+        start_process: true,
+        server_path: require('geckodriver').path,
+        port: 9515
+      },
+      desiredCapabilities: FIREFOX
+    },
     selenium: {
+      // Selenium Server is running locally and is managed by Nightwatch
+      selenium: {
+        start_process: true,
+        port: 4444,
+        server_path: require('selenium-server').path,
+        cli_args: {
+          'webdriver.gecko.driver': require('geckodriver').path,
+          'webdriver.chrome.driver': require('chromedriver').path
+        }
+      }
+    },
+    seleniumChrome: {
+      extends: 'selenium',
+      desiredCapabilities: CHROME
+    },
+    seleniumFirefox: {
+      extends: 'selenium',
+      desiredCapabilities: FIREFOX
+    },
+    seleniumHub: {
       // Selenium Server is running locally and is managed by Nightwatch
       selenium: {
         port: 4444,
         host: 'triton927.startdedicated.de'
-      },
-      webdriver: {
-        start_process: false,
-        port: 4444
       }
     },
-    'selenium.chrome': {
-      extends: 'selenium',
-      desiredCapabilities: {
-        browserName: 'chrome',
-        chromeOptions: {
-          w3c: false
-        }
-      }
+    seleniumHubChrome: {
+      extends: 'seleniumHub',
+      desiredCapabilities: CHROME
     },
-    'selenium.firefox': {
-      extends: 'selenium',
-      desiredCapabilities: {
-        browserName: 'firefox'
-      }
+    seleniumHubFirefox: {
+      extends: 'seleniumHub',
+      desiredCapabilities: FIREFOX
     }
   }
 };
