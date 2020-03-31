c19fe19d9f87e031ffab56aca66c61ea08ac7aa0
Martin Oppitz <martinoppitz@users.noreply.github.com>
2020-03-31 14:54:43 +0200

    Update app-test.js

--- tests/e2e/app-test.js
+++ tests/e2e/app-test.js
@@ -45,16 +45,7 @@ function testFall(url) {
 }
 
 const tests = {};
-[
-//   'https://github.modevel.de/poc/angular.html',
-//   'https://github.modevel.de/poc/angularjs.html',
-//   'https://github.modevel.de/poc/aurelia.html',
-//   'https://github.modevel.de/poc/inferno.html',
-  'https://github.modevel.de/poc/preact.html'
-//   'https://github.modevel.de/poc/react.html',
-//   'https://github.modevel.de/poc/svelte.html',
-//   'https://github.modevel.de/poc/vue.html'
-].forEach(url => {
+['https://github.modevel.de/poc/preact.html'].forEach(url => {
   tests[url] = testFall(url);
 });
 module.exports = tests;
