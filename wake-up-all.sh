acb0a614eb31e3025935516070c13e258f7d9a30
Martin Oppitz <martinoppitz@users.noreply.github.com>
2020-10-10 20:03:58 +0200

    Update wake-up-all.sh

--- wake-up-all.sh
+++ wake-up-all.sh
@@ -1,7 +1,7 @@
 cp -r public dist
 sh ./wake-up.sh angular $1
 sh ./wake-up.sh angularjs $1
-sh ./wake-up.sh aurelia $1
+# sh ./wake-up.sh aurelia $1
 sh ./wake-up.sh inferno $1
 sh ./wake-up.sh preact $1
 sh ./wake-up.sh react $1
