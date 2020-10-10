e9bb75e7ac56a2946fad9dde8f9c982fc8134df8
Martin Oppitz <martinoppitz@users.noreply.github.com>
2020-10-10 20:17:19 +0200

    Update wake-up-all.sh

--- wake-up-all.sh
+++ wake-up-all.sh
@@ -5,7 +5,7 @@ sh ./wake-up.sh angularjs $1
 sh ./wake-up.sh inferno $1
 sh ./wake-up.sh preact $1
 sh ./wake-up.sh react $1
-sh ./wake-up.sh svelte $1
+# sh ./wake-up.sh svelte $1
 sh ./wake-up.sh vanilla $1
 sh ./wake-up.sh vue $1
 sh ./wake-up.sh vue3 $1
