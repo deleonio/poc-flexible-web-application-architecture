0623866eb08f6b8be2d66f9cdd54e0146d0c2dd9
deleonio <6279703+deleonio@users.noreply.github.com>
2020-12-15 16:03:25 +0100

    Update wake-up.sh

--- wake-up.sh
+++ wake-up.sh
@@ -1,8 +1,8 @@
 mkdir workspace || { echo 'command failed' ; exit 1; }
 cd workspace || { echo 'command failed' ; exit 1; }
-npm install --legacy-peer-deps @leanup/cli@$2 @leanup/cli-$1@$2 --no-save || { echo 'command failed' ; exit 1; }
-# npx lean create -o -n template || { echo 'command failed' ; exit 1; }
-npx $1 create -o -n template || { echo 'command failed' ; exit 1; }
+npm install @leanup/cli@$2 @leanup/cli-$1@$2 --no-save || { echo 'command failed' ; exit 1; }
+# npx lean create -n template || { echo 'command failed' ; exit 1; }
+npx $1 create -n template || { echo 'command failed' ; exit 1; }
 # npm update --legacy-peer-deps || { echo 'command failed' ; exit 1; }
 # npx lean lint || { echo 'command failed' ; exit 1; }
 npx $1 lint || { echo 'command failed' ; exit 1; }
@@ -11,7 +11,7 @@ npx $1 format || { echo 'command failed' ; exit 1; }
 # npx lean test || { echo 'command failed' ; exit 1; }
 npx $1 test || { echo 'command failed' ; exit 1; }
 # npx lean coverage || { echo 'command failed' ; exit 1; }
-# npx $1 coverage || { echo 'command failed' ; exit 1; }
+npx $1 coverage || { echo 'command failed' ; exit 1; }
 # npx lean build --gzip || { echo 'command failed' ; exit 1; }
 npx $1 build || { echo 'command failed' ; exit 1; }
 # cp dist/app.css ../dist/app.css || { echo 'command failed' ; exit 1; }
