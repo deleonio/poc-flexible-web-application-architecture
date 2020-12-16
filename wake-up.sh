19df0029c09d3a832423b0f29d444c0773e83281
deleonio <6279703+deleonio@users.noreply.github.com>
2020-12-16 22:59:52 +0100

    Update wake-up.sh

--- wake-up.sh
+++ wake-up.sh
@@ -1,23 +1,25 @@
 mkdir workspace || { echo 'command failed' ; exit 1; }
 cd workspace || { echo 'command failed' ; exit 1; }
 npm install --no-save --global @leanup/cli@$2 @leanup/cli-$1@$2 || { echo 'command failed' ; exit 1; }
-# npx lean create -n template || { echo 'command failed' ; exit 1; }
+# lean create -n template || { echo 'command failed' ; exit 1; }
 $1 create -n template || { echo 'command failed' ; exit 1; }
-# npm update --legacy-peer-deps || { echo 'command failed' ; exit 1; }
-npm install --no-save @leanup/cli@$2 @leanup/cli-$1@$2 || { echo 'command failed' ; exit 1; }
 # npx lean lint || { echo 'command failed' ; exit 1; }
-npx $1 lint || { echo 'command failed' ; exit 1; }
+# npx $1 lint || { echo 'command failed' ; exit 1; }
+npm run lint || { echo 'command failed' ; exit 1; }
 # npx lean format || { echo 'command failed' ; exit 1; }
-npx $1 format || { echo 'command failed' ; exit 1; }
+# npx $1 format || { echo 'command failed' ; exit 1; }
+npm run format || { echo 'command failed' ; exit 1; }
 # npx lean test || { echo 'command failed' ; exit 1; }
-npx $1 test || { echo 'command failed' ; exit 1; }
+# npx $1 test || { echo 'command failed' ; exit 1; }
+npm run test || { echo 'command failed' ; exit 1; }
 # npx lean coverage || { echo 'command failed' ; exit 1; }
-npx $1 coverage || { echo 'command failed' ; exit 1; }
+# npx $1 coverage || { echo 'command failed' ; exit 1; }
+npm run coverage || { echo 'command failed' ; exit 1; }
 # npx lean build --gzip || { echo 'command failed' ; exit 1; }
-npx $1 build || { echo 'command failed' ; exit 1; }
+# npx $1 build || { echo 'command failed' ; exit 1; }
+npm run build || { echo 'command failed' ; exit 1; }
 # cp dist/app.css ../dist/app.css || { echo 'command failed' ; exit 1; }
 cp dist/app.js ../dist/$1.js || { echo 'command failed' ; exit 1; }
-echo "Baum"
 cp -r dist/assets ../dist/ || { echo 'command failed' ; exit 1; }
 # cp dist/app.js.gz ../dist/$1.js.gz || { echo 'command failed' ; exit 1; }
 # npx lean serve -o -m production || { echo 'command failed' ; exit 1; }
