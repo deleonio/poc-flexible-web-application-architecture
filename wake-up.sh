7415765a356487d1299b4ff1c25d681c9d808481
deleonio <6279703+deleonio@users.noreply.github.com>
2020-12-20 08:50:33 +0100

    Update wake-up.sh

--- wake-up.sh
+++ wake-up.sh
@@ -19,7 +19,7 @@ npx lean build || { echo 'command failed' ; exit 1; }
 npx $1 build || { echo 'command failed' ; exit 1; }
 npm run build || { echo 'command failed' ; exit 1; }
 cp dist/app.js ../dist/$1.js || { echo 'command failed' ; exit 1; }
-cp dist/app.css ../dist/$1.css || true
+cp dist/app.css ../dist/style.css || true
 cp -r dist/assets ../dist/ || { echo 'command failed' ; exit 1; }
 # npx lean serve -o -m production || { echo 'command failed' ; exit 1; }
 # npx $1 serve -o -m production || { echo 'command failed' ; exit 1; }
