ebd2a77522f294b6f845483705a8c75555713881
deleonio <6279703+deleonio@users.noreply.github.com>
2021-01-09 16:27:17 +0100

    Update wake-up.sh

--- wake-up.sh
+++ wake-up.sh
@@ -22,9 +22,9 @@ npm run build || { echo 'command failed' ; exit 1; }
 cp dist/app.js ../dist/$1.js || { echo 'command failed' ; exit 1; }
 # cp dist/app.css ../dist/style.css || true
 cp -r dist/assets ../dist/ || { echo 'command failed' ; exit 1; }
-npx lean serve --port 8080 & || { echo 'command failed' ; exit 1; }
-npx $1 serve --port 8081 & || { echo 'command failed' ; exit 1; }
-npx lean serve -- --port 8082 & || { echo 'command failed' ; exit 1; }
+npx lean serve --port 8080 &
+npx $1 serve --port 8081 &
+npx lean serve -- --port 8082 &
 # npx lean e2e -e chrome || { echo 'command failed' ; exit 1; }
 # npx $1 e2e -e chrome || { echo 'command failed' ; exit 1; }
 npm outdated || true
