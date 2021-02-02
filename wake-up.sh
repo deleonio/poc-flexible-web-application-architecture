edade9fa3c0c709030757bfe80e8627fd2566ba7
deleonio <6279703+deleonio@users.noreply.github.com>
2021-02-02 05:00:07 +0100

    Update wake-up.sh

--- wake-up.sh
+++ wake-up.sh
@@ -21,13 +21,15 @@ npm run coverage || { echo 'command failed' ; exit 1; }
 npx lean build || { echo 'command failed' ; exit 1; }
 npx $1 build || { echo 'command failed' ; exit 1; }
 npm run build || { echo 'command failed' ; exit 1; }
-# npm run build:snowpack || true
+npm run build:snowpack || true
 mkdir ../dist/$1
 cp -r dist/* ../dist/$1 || { echo 'command failed' ; exit 1; }
 mkdir ../dist/$1/$1
 cp -r dist/* ../dist/$1/$1 || { echo 'command failed' ; exit 1; }
 cp -r dist/assets ../dist/ || { echo 'command failed' ; exit 1; }
 cp -r dist/assets ../dist/$1 || { echo 'command failed' ; exit 1; }
+cp -r src/assets ../dist/ || { echo 'command failed' ; exit 1; }
+cp -r src/assets ../dist/$1 || { echo 'command failed' ; exit 1; }
 npm run build:snowpack || true
 # npx lean serve --port 8080 &
 # npx $1 serve --port 8081 &
