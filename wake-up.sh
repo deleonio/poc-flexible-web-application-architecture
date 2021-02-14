ad707dac71bce7b7e41a6b48b2be5e8920b629d8
deleonio <6279703+deleonio@users.noreply.github.com>
2021-02-14 07:51:05 +0100

    Update wake-up.sh

--- wake-up.sh
+++ wake-up.sh
@@ -21,11 +21,11 @@ npm run coverage || { echo 'command failed' ; exit 1; }
 npx lean build || { echo 'command failed' ; exit 1; }
 npx $1 build || { echo 'command failed' ; exit 1; }
 npm run build || { echo 'command failed' ; exit 1; }
+mv dist/app.js dist/main.js || true
 # npm run build:snowpack || true
 mkdir ../dist/$1
 cp -r dist/* ../dist/$1 || { echo 'command failed' ; exit 1; }
 mkdir ../dist/$1/$1
-mv dist/app.js dist/main.js || true
 cp -r dist/* ../dist/$1/$1 || { echo 'command failed' ; exit 1; }
 cp -r dist/assets ../dist/ || { echo 'command failed' ; exit 1; }
 cp -r dist/assets ../dist/$1 || { echo 'command failed' ; exit 1; }
